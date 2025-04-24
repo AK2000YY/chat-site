b28eb3c7388732c8acb8e0cdb22c290a8db4500a
Abdulkarim <abdyabkor@gmail.com>
2025-04-24 16:56:40 +0300

    some refactors

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import { useContext, useEffect, useState } from "react";
+import axiosInc from "./utils/axios";
+import { AuthContext } from "./context/AuthContext";
+import Loading from "./componenets/ui/Loading";
+import { Toaster } from "react-hot-toast";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [isLoading, setIsLoading] = useState<Boolean>(true);
+  const { user, setUser } = useContext(AuthContext)!;
+
+  useEffect(() => {
+    const getAuthState = async () => {
+      try {
+        const user: any = await axiosInc.get("/auth/auth-state");
+        setUser(user.data)
+      } catch (e: any) {
+        if (e.response) {
+          console.log(e.response.data.message);
+        } else if (e.request) {
+          console.log("No response from server. Server may be down.");
+        } else {
+          console.log("Unexpected error:", e.message);
+        }
+      }
+      setIsLoading(false);
+    }
+    if (!user._id)
+      getAuthState();
+  }, [])
+
+  if (isLoading)
+    return <Loading />
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Routes>
+        <Route path="/login" element={!user._id ? <Login /> : < Navigate to={"/"} />} />
+        <Route path="/Signup" element={!user._id ? <Signup /> : < Navigate to={"/"} />} />
+        <Route path="/" element={!user._id ? <Home /> : < Navigate to={"/login"} />} />
+      </Routes>
+      <Toaster />
+    </div>
+  );
 }
 
-export default App
+export default App;

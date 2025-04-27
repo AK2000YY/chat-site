134213ea2acea06c8cb3bf47d272fec4a9382900
Abdulkarim <abdyabkor@gmail.com>
2025-04-27 22:32:12 +0300

    add pendding to button

--- src/componenets/ui/Button.tsx
+++ src/componenets/ui/Button.tsx
@@ -1,13 +1,16 @@
 import { ButtonHTMLAttributes, FC } from "react"
 import cn from "../../utils/functions"
+import { useFormStatus } from "react-dom";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> { }
 
 const Button: FC<ButtonProps> = ({ className, ...props }) => {
+    const status = useFormStatus();
     const myStyle = "bg-slate-400 w-full py-3 rounded-xl text-white font-bold text-xl";
     return (
         <button
             className={cn(myStyle, className)}
+            disabled={status.pending}
             {...props}
         />
     )

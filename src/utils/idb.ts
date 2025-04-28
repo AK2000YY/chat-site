9f47112125644323d8a3937f81185cc13bfa2bf5
Abdulkarim <abdyabkor@gmail.com>
2025-04-28 23:33:20 +0300

    setup idb and setup function for pagination

---
+++ src/utils/idb.ts
@@ -0,0 +1,113 @@
+import { openDB, IDBPDatabase } from 'idb';
+
+
+type Message = {
+    _id: string,
+    friend: string,
+    senderType: string,
+    message?: string,
+    media?: string
+}
+
+
+const DB_NAME = 'chat';
+const STORE_NAME = 'messages';
+const VERSION = 1;
+const LIMIT = 30;
+
+let dbPromise: Promise<IDBPDatabase>;
+
+const initDB = async (): Promise<IDBPDatabase> => {
+    if (!dbPromise) {
+        dbPromise = openDB(DB_NAME, VERSION, {
+            upgrade(db) {
+                if (!db.objectStoreNames.contains(STORE_NAME)) {
+                    const store = db.createObjectStore(STORE_NAME, { keyPath: '_id' });
+                    store.createIndex("friend_id", ["friend", "_id"]);
+                }
+            },
+        });
+    }
+    return dbPromise;
+};
+
+const addMessage = async (messages: Message[]) => {
+    const db = await initDB();
+    const tx = db.transaction('messages', 'readwrite');
+    const store = tx.objectStore('messages');
+    for (const message of messages) {
+        await store.put(message);
+    }
+    await tx.done;
+};
+
+const updateMessage = async (message: Message): Promise<void> => {
+    const db = await initDB();
+    const tx = db.transaction(STORE_NAME, 'readwrite');
+    await tx.store.put(message);
+    await tx.done;
+};
+
+const deleteMessage = async (_id: string): Promise<void> => {
+    const db = await initDB();
+    await db.delete(STORE_NAME, _id);
+};
+
+const getBeforeId = async (friendId: string, messageId?: string): Promise<Message[]> => {
+    const messages: Message[] = [];
+    const db = await initDB();
+    const tr = db.transaction(STORE_NAME);
+    const store = tr.objectStore(STORE_NAME);
+    const index = store.index("friend_id");
+    let range;
+
+    if (!messageId)
+        range = IDBKeyRange.lowerBound([friendId, '\uffff'])
+    else
+        range = IDBKeyRange.bound(
+            [friendId, ''],
+            [friendId, messageId],
+            false,
+            true
+        );
+
+    let cursor = await index.openCursor(range, 'prev');
+
+    while (cursor && messages.length < LIMIT) {
+        messages.push(cursor.value);
+        cursor = await cursor.continue();
+    }
+
+    return messages;
+}
+
+const getAfterId = async (friendId: string, messageId?: string): Promise<Message[]> => {
+    const messages: Message[] = [];
+    const db = await initDB();
+    const tr = db.transaction(STORE_NAME);
+    const store = tr.objectStore(STORE_NAME);
+    const index = store.index("friend_id");
+    let range;
+
+    if (!messageId)
+        range = IDBKeyRange.upperBound([friendId, ''])
+    else
+        range = IDBKeyRange.bound(
+            [friendId, messageId],
+            [friendId, '\uffff'],
+            true,
+            false
+        );
+
+    let cursor = await index.openCursor(range, 'next');
+
+    while (cursor && messages.length < LIMIT) {
+        messages.push(cursor.value);
+        cursor = await cursor.continue();
+    }
+
+    return messages;
+}
+
+
+export { addMessage, updateMessage, deleteMessage, getBeforeId, getAfterId }

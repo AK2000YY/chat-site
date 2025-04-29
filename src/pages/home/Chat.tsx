96b835e2f0ad9f44e839fac1a1e208706ed37fe4
Abdulkarim <abdyabkor@gmail.com>
2025-04-29 16:43:43 +0300

    some refactor

--- src/pages/home/Chat.tsx
+++ src/pages/home/Chat.tsx
@@ -1,7 +1,18 @@
+import { useContext, useEffect, useState } from "react"
+import { chatContext } from "../../context/ChatContext"
+import Message from "../../types/message";
+
 const Chat = () => {
+    const { chatInfo } = useContext(chatContext)!;
+    const [messages, setMessages] = useState<Message[]>([]);
+
+    useEffect(() => {
+
+    }, [])
+
     return (
         <div
-            className="w-[100%] h-[90%] sm:w-[60%] sm:h-[100%] lg:w-[50%] bg-slate-900 rounded-xl"
+            className="w-[100%] h-[90%] sm:w-[60%] sm:h-[100%] lg:w-[50%] bg-[#110e21] rounded-xl"
         >
             Chat
         </div>
--- src/pages/home/FriendProfile.tsx
+++ src/pages/home/FriendProfile.tsx
@@ -1,7 +1,7 @@
 const FriendProfile = () => {
     return (
         <div
-            className="w-[100%] h-[90%] sm:w-[60%] sm:h-[100%] lg:w-[20%] bg-slate-900 rounded-xl"
+            className="w-[100%] h-[90%] sm:w-[60%] sm:h-[100%] lg:w-[20%] bg-[#110e21] rounded-xl"
         >
             FriendProfile
         </div>

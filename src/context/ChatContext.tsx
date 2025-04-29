51a7efee3527b3b2fe217324fe8e44f3c6e0e782
Abdulkarim <abdyabkor@gmail.com>
2025-04-29 16:09:57 +0300

    add chat context

---
+++ src/context/ChatContext.tsx
@@ -0,0 +1,27 @@
+import { createContext, ReactNode, useState } from "react";
+import ChatInfo from "../types/chatInfo";
+
+
+const chatContext = createContext<{
+    chatInfo: ChatInfo,
+    setChatInfo: React.Dispatch<React.SetStateAction<ChatInfo>>
+} | undefined>(undefined);
+
+
+const ChatProvider = ({ children }: {
+    children: ReactNode
+}) => {
+    const [chatInfo, setChatInfo] = useState<ChatInfo>({
+        changes: [],
+        selectedUser: "",
+        isLoading: true
+    });
+
+    return (
+        <chatContext.Provider value={{ chatInfo, setChatInfo }}>
+            {children}
+        </chatContext.Provider>
+    )
+}
+
+export { chatContext, ChatProvider };

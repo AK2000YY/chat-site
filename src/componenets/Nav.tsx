db38c127930c9dd6c6d0b993cbd0a725990d8c95
Abdulkarim <abdyabkor@gmail.com>
2025-05-06 23:12:32 +0300

    refactor components

--- src/componenets/Nav.tsx
+++ src/componenets/Nav.tsx
@@ -1,22 +1,37 @@
 import { IoExitOutline } from "react-icons/io5";
-import { RxPerson } from "react-icons/rx";
-import { LiaUserFriendsSolid } from "react-icons/lia";
 import { MdOutlineSettings } from "react-icons/md";
 import { IoMdSearch } from "react-icons/io";
 import cn from "../utils/functions";
+import { FaUserFriends } from "react-icons/fa";
+import { TbHomeFilled } from "react-icons/tb";
+import { useState } from "react";
 
 
 
 const Nav = () => {
-    const iconStyle = "size-6 mb-3"
+    const [selected, setSelected] = useState<number>(0)
     return (
         <div className="w-[100%] sm:w-[6%] md:w-[5%] lg:w-[4%] bg-[#110e21] text-white rounded-xl p-3 pb-0 flex sm:flex-col justify-between items-center shadow-2xl">
-            <RxPerson className={iconStyle} />
-            <LiaUserFriendsSolid className={iconStyle} />
-            <IoMdSearch className={iconStyle} />
+            <TbHomeFilled
+                className={cn("size-9 p-1 mb-3 rounded-md shadow cursor-pointer", { 'bg-[#9e7ffa]': selected === 0 })}
+                onClick={() => setSelected(0)}
+            />
+            <FaUserFriends
+                className={cn("size-9 p-1 mb-3 rounded-md  shadow cursor-pointer", { 'bg-[#9e7ffa]': selected === 1 })}
+                onClick={() => setSelected(1)}
+            />
+            <IoMdSearch
+                className={cn("size-9 p-1 mb-3 rounded-md shadow cursor-pointer", { 'bg-[#9e7ffa]': selected === 2 })}
+                onClick={() => setSelected(2)}
+            />
             <div className="hidden sm:block sm:grow"></div>
-            <MdOutlineSettings className={iconStyle} />
-            <IoExitOutline className={cn(iconStyle, "text-red-600")} />
+            <MdOutlineSettings
+                className={cn("size-9 p-1 mb-3 rounded-md shadow cursor-pointer", { 'bg-[#9e7ffa]': selected === 3 })}
+                onClick={() => setSelected(3)}
+            />
+            <IoExitOutline
+                className={cn("size-9 mb-3 rounded-md shadow cursor-pointer", "pl-1 text-red-600")}
+            />
         </div>
     )
 }
--- src/componenets/ui/Avatar.tsx
+++ src/componenets/ui/Avatar.tsx
@@ -1,15 +1,20 @@
+import { FC, ImgHTMLAttributes } from "react"
 import { RxPerson } from "react-icons/rx"
+import cn from "../../utils/functions"
 
-const Avatar = ({ image }: {
+interface AvatarProp extends ImgHTMLAttributes<HTMLImageElement> {
     image: string
-}) => {
+}
+
+const Avatar: FC<AvatarProp> = ({ className, image }) => {
     return (
         <div>
             {
                 image === "" ?
-                    <RxPerson className="text-white border-1 rounded-full size-6" /> :
+                    <RxPerson className={"text-white p-1 border-1 rounded-full"} /> :
                     <img
-                        src={image}
+                        className={cn("size-10 rounded-full", className)}
+                        src={"http://localhost:5000/uploads/" + image}
                     />
             }
         </div>

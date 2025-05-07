0ea5dac5a0a40cf1f9bf97ff1a1ea0c3f69b4742
Abdulkarim <abdyabkor@gmail.com>
2025-05-08 02:06:35 +0300

    refactor input component

--- src/componenets/form/Input.tsx
+++ src/componenets/form/Input.tsx
@@ -3,7 +3,7 @@ import cn from "../../utils/functions";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> { }
 
-const Input: FC<InputProps> = ({ className, ...props }) => {
+const Input: FC<InputProps> = ({ className, children, ...props }) => {
     const myStyle = "w-full h-15 px-2 rounded-md text-white shadow-2xs bg-slate-700";
     return (
         <div

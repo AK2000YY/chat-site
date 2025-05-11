69122e99cd8069b2380e0e322701c3035dd190d7
Abdulkarim <abdyabkor@gmail.com>
2025-05-11 22:22:27 +0300

    finish friend page

--- src/pages/friends/Friends.tsx
+++ src/pages/friends/Friends.tsx
@@ -1,6 +1,24 @@
+import { useState } from "react"
+import SearchFriends from "./SearchFriends"
+import SearchResult from "./SearchResult"
+import Friend from "../../types/friend";
+
 const Friends = () => {
+    const [searchResault, setSearchResult] = useState<Friend[]>([]);
+
+    const handleSearch = (friends: Friend[]) => {
+        setSearchResult(friends);
+    }
+
     return (
-        <div>Friends</div>
+        <div className="bg-[#110e21] w-[100%] h-[100%] sm:w-[50%] lg:w-[40%] mx-auto mb-3 sm:mb-0 rounded-xl p-3 self-center">
+            <SearchFriends
+                setFriends={handleSearch}
+            />
+            <SearchResult
+                friends={searchResault}
+            />
+        </div>
     )
 }
 
